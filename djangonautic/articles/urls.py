e0fdf34eb5ccad617e515056a40e103fb73743fb
Yolanda <yolmabot021@student.wethinkcode.co.za>
2023-04-19 20:02:35 +0200

    set up the urls to campture the slug

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from .import views
+# from django.urls import re_path as url
+
 
 urlpatterns = [
-    path('',views.article_list)
+    path('',views.article_list),
+    re_path('(?P<slug>[\w-]+)/', views.article_detail),
 ]
